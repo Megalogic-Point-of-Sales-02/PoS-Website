d5fff404206b136367ec6c9a598ebb25e5d67377
raihan.kus <raihan.kus@ui.ac.id>
2024-05-29 23:07:36 +0700

    fix resetpassword with suspense

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,7 +1,11 @@
 import ResetPassword from "@/modules/Authentication/ForgotPassword/ResetPassword";
-
+import { Suspense } from "react";
 const page = () => {
-  return <ResetPassword />;
+  return (
+    <Suspense>
+      <ResetPassword />;
+    </Suspense>
+  );
 };
 
 export default page;
