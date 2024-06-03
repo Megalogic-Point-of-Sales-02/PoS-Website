c9554b1f5114393ad538cd6acaa5b4d64c45ce34
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2024-06-03 21:31:50 +0700

    use dropdown for product

---
+++ src/utils/categoryProduct.ts
@@ -0,0 +1,60 @@
+export const CategoriesList = {
+      'Vehicles' : [
+        'Cars',
+        'Motorcycles',
+        'Trucks & Trailers',
+        'Boats & Watercraft',
+        'Bicycles',
+        'Spare Parts & Accessories',
+        'Other',
+      ],
+
+      'Mobile Phone & Tablets': [
+        'Mobile Phones',
+        'Tablets',
+        'Phone Numbers',
+        'Phone Cases & Screen Protectors',
+        'Phone Batteries & Chargers',
+        'SIM Cards',
+        'Other',
+      ],
+
+      'Electronics': [
+        'Computers & Laptops',
+        'TV, Audio & Video',
+        'Cameras & Accessories',
+        'Video Games & Consoles',
+        'Printers & Scanners',
+        'Other',
+      ],
+
+      'Furniture & Appliance': [
+        'Furniture',
+        'Home Appliances',
+        'Kitchen & Dining',
+        'Decorations, Garden & Accessories',
+        'Other',
+      ],
+
+      'Health & Beauty': [
+        'Health Care',
+        'Skin & Hair',
+        'Fragrance',
+        'Makeup',
+        'Vitamins & Supplements',
+        'Other',
+      ],
+
+      'Fashion' : [
+        'Clothing',
+        'Shoes',
+        'Bags & Accessories',
+        'Jewelry',
+        'Watches',
+        'Other',
+      ],
+
+      'Others' : [
+        'Other',
+      ],
+};
